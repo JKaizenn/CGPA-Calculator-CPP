ac372f56b4279ef3752827d91a0186924b4f53e0
Jessen Forbush <jessenforbush@gmail.com>
2026-03-03 22:04:18 -0500

    started a fileReader class for reading student and course data from csv and other file types.

---
+++ include/fileReader.h
@@ -0,0 +1,45 @@
+/***********************************************************************
+ * Header File:
+ *    File Reader
+ * Author:
+ *    Jessen
+ * Summary:
+ *    Everything we need to know about a File Reader
+ ************************************************************************/
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <map>
+
+/************************************
+ * FILE READER
+ ************************************/
+class FileReader
+{
+public:
+   // Default Constructor
+   FileReader(std::string& studentFileName, std::string& courseFileName) {}
+
+   // Read File
+
+
+   // Create File
+
+
+   // Update File
+
+
+   // Delete File
+
+private:
+   std::string studentFileName {"students.csv"}
+   std::string courseFileName  {"courses.csv" }
+
+
+
+
+};
